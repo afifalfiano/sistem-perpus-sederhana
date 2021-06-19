5f9b04a8529ebbb13784f86c958a40f8ca670edd
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-06-19 16:08:55 +0700

    fix

--- visitor/buku-visitor/buku-visitor.php
+++ visitor/buku-visitor/buku-visitor.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sistem Perpustakaan</title>
     <link rel="stylesheet" href="../../styles/bootstrap.min.css">
     <link rel="stylesheet" href="../../styles/styles.css">
     <script src="../../scripts/bootstrap.min.js"></script>
